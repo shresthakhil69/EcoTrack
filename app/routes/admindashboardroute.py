8c4a9be6db616d306478075dcbbe0319880b1d59
Shresthakhil69 <shresthakhil69@gmail.com>
2026-06-19 12:11:55 +0545

    add admin session validation

--- app/routes/admindashboardroute.py
+++ app/routes/admindashboardroute.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
 
 admin_dashboardBP = Blueprint("admin_dashboard", __name__)
+
+def admin_required():
+    return 'admin_id' not in session
+
 
 @admin_dashboardBP.route('/admin/dashboard')
 def dashboard():
